c1af0db5fc2105737cd34204c0cc6ebc82b9065a
Mark Castillo <markian.castillo@benilde.edu.ph>
2018-03-19 02:04:29 +0800

    Update 7.7.3
    Updated the config file for the GearHost database.

--- controllers/config.php
+++ controllers/config.php
@@ -1,9 +1,9 @@
 <?php
 #	Follows the syntax: $servername = "serverName/instanceName";
-	$serverName = "localhost";
+	$serverName = "den1.mssql2.gear.host";
 
-#	$connectionInfo  = array("Database"=>"database name", "UID"=>"db username", "PWD"=>"db password");
+	$connectionInfo  = array("Database"=>"aurum", "UID"=>"aurum", "PWD"=>"damong_talahiban");
 #	If password is not specified, connection will be attempted through windows authentication
-	$connectionInfo = array("Database" => "aurum");
+#	$connectionInfo = array("Database" => "aurum");
 	$con = sqlsrv_connect($serverName, $connectionInfo);
 ?>
