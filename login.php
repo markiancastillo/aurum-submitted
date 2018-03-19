9b9ba1f4cf301f29f5a7d5eb89eb05e150dbe2d4
Mark Castillo <markian.castillo@benilde.edu.ph>
2018-03-19 10:27:03 +0800

    Update 7.7.5
    Trial 2

--- login.php
+++ login.php
@@ -5,10 +5,7 @@ include('controllers/forgot_password_controller.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="encoding">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8" />
 
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
